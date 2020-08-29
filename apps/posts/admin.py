ba72c4e9cf3901f2222dd7b48c99f317cda7046d
vinhbui107 <vinhbui.tg@gmail.com>
2020-08-29 15:16:47 +0700

    show model in admin page

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post, Like, Comment, Reply
+
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(Reply)
