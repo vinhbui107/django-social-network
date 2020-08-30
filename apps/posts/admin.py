7566b959c4cd387055eecff77e1a716987cfb8cb
vinhbui107 <vinhbui.tg@gmail.com>
2020-08-30 15:06:45 +0700

    remove import model i just deleted

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Post, Like, Comment, Reply
+from .models import Post, Comment, Reply
 
 
 # Register your models here.
 admin.site.register(Post)
-admin.site.register(Like)
 admin.site.register(Comment)
 admin.site.register(Reply)
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser, Follow
+from .models import CustomUser
 
 
 admin.site.register(CustomUser)
-admin.site.register(Follow)
