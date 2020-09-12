c43486ce63d6fa9b198b1f3c6dc53bdf06f92025
vinhbui107 <vinhbui.tg@gmail.com>
2020-09-12 22:06:38 +0700

    Update config for static and media

--- myproject/settings.py
+++ myproject/settings.py
@@ -218,6 +218,11 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
