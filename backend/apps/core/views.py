c6c31dedc790eea48b881667aeb3ce63f3c56774
vinhbui107 <vinhbui.tg@gmail.com>
2020-10-26 21:24:14 +0700

    Remove code unneed

--- backend/apps/core/views.py
+++ backend/apps/core/views.py
@@ -1,15 +1,3 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from apps.posts.models import Post, Reply, Comment
-from apps.users.models import CustomUser
 
-
-class HomeView(LoginRequiredMixin, TemplateView):
-    login_url = "users:login"
-    template_name = "core/home.html"
-    redirect_field_name = "redirect_to"
-
-    def get(self, request, *args, **kwargs):
-        posts = Post.objects.all()
-        return render(request, self.template_name, {"posts": posts})
+# Create your views here.
