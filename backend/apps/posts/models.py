276b962d68caf5a9add930e3c681280fdaa4c984
vinhbui107 <vinhbui.tg@gmail.com>
2020-11-02 22:18:22 +0700

    Create models: Post, Hastag, Comment, Reply

--- backend/apps/posts/models.py
+++ backend/apps/posts/models.py
@@ -1,27 +1,40 @@
 from django.db import models
-from apps.users.models import CustomUser
 from django.utils.crypto import get_random_string
 from django.utils import timezone
+from backend.apps.users.models import CustomUser
+
+
+class Hashtag(models.Model):
+    tag_name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.tag_name
 
 
 class Post(models.Model):
     user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     content = models.TextField()
-    photo = models.ImageField(upload_to="post_images/", blank=True)
+    photo = models.ImageField(upload_to="post_images/", blank=True, null=True)
     slug = models.SlugField(default="", unique=True)
     is_hidden = models.BooleanField(default=False)
-    created_date = models.DateTimeField()
-    updated_date = models.DateTimeField()
     likes = models.ManyToManyField(
-        CustomUser, related_name="like_users", blank=True, symmetrical=False,
+        CustomUser,
+        related_name="post_likes",
+        symmetrical=False,
     )
+    hashtags = models.ManyToManyField(
+        Hashtag,
+        related_name="post_hashtags",
+        symmetrical=False,
+    )
+
+    def __str__(self):
+        return self.content
 
     def save(self, *args, **kwargs):
         if not self.pk:
             self.slug = create_slug()
-            self.created_date = timezone.now()
 
-        self.updated_date = timezone.now()
         return super(Post, self).save(*args, **kwargs)
 
 
@@ -33,15 +46,9 @@ class Comment(models.Model):
         CustomUser, on_delete=models.CASCADE, related_name="user_commented"
     )
     content = models.TextField()
-    created_date = models.DateTimeField()
-    updated_date = models.DateTimeField()
 
-    def save(self, *args, **kwargs):
-        if not self.pk:
-            self.created_date = timezone.now()
-
-        self.updated_date = timezone.now()
-        return super(Comment, self).save(*args, **kwargs)
+    def __str__(self):
+        return self.content
 
 
 class Reply(models.Model):
@@ -52,15 +59,9 @@ class Reply(models.Model):
         CustomUser, on_delete=models.CASCADE, related_name="user_replied"
     )
     content = models.TextField()
-    created_date = models.DateTimeField()
-    updated_date = models.DateTimeField()
 
-    def save(self, *args, **kwargs):
-        if not self.pk:
-            self.created_date = timezone.now()
-
-        self.updated_date = timezone.now()
-        return super(Reply, self).save(*args, **kwargs)
+    def __str__(self):
+        return self.content
 
 
 def create_slug():
